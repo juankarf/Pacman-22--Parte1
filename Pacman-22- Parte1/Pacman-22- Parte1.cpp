03e936d7cd2f06bf231a36dafe6db82ed4d5b75d
juankarf <jcrfaudiovisual@hotmail.com>
2022-12-06 15:19:34 +0100

    MOVIMIENTO PERSONAJE

--- Pacman-22- Parte1/Pacman-22- Parte1.cpp
+++ Pacman-22- Parte1/Pacman-22- Parte1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #define MAP_VERTICAL 29
 #define MAP_HORIZONTAL 120
+#define PERSONAJE '0'
 enum TILES {EMPTY = ' ', WALL = '#', POINT = '.' };
 TILES map[MAP_VERTICAL][MAP_HORIZONTAL];
 bool run = true; 
@@ -8,7 +9,14 @@ bool run = true;
 enum INPUT {QUIT, UP, DOWN, LEFT, RIGHT, UNKNOWN };
 INPUT currentInput = INPUT::UNKNOWN; 
 
+int personaje_x;
+int personaje_y;
+int puntuacion_actual = 0;
+int puntuacion_total;
+
 void Setup() {
+	personaje_x = MAP_HORIZONTAL / 2;
+	personaje_y = MAP_VERTICAL / 2;
 	for (size_t i = 0; i < MAP_VERTICAL; i++)
 	{
 		for (size_t j = 0; j < MAP_HORIZONTAL; j++)
@@ -21,6 +29,36 @@ void Setup() {
 			}
 		}
 	}
+
+	map[10][10] = TILES::POINT;
+	map[10][11] = TILES::POINT;
+	map[10][12] = TILES::POINT;
+
+	map[11][0] = TILES::EMPTY;
+	map[12][0] = TILES::EMPTY;
+	map[13][0] = TILES::EMPTY;
+	map[14][0] = TILES::EMPTY;
+	map[0][56] = TILES::EMPTY;
+	map[0][57] = TILES::EMPTY;
+	map[0][58] = TILES::EMPTY;
+	map[0][59] = TILES::EMPTY;
+	map[0][60] = TILES::EMPTY;
+	map[0][61] = TILES::EMPTY;
+	map[0][62] = TILES::EMPTY;
+	map[0][63] = TILES::EMPTY;
+
+	map[10][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[11][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[12][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[13][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][56] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][57] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][58] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][59] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][60] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][61] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][62] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][63] = TILES::EMPTY;
 }
 
 
@@ -53,32 +91,63 @@ void Input() {
 		currentInput = INPUT::UNKNOWN;
 		break;
 	}
-
 }
 void Logic() {
+	int personaje_y_new = personaje_y;
+	int personaje_x_new = personaje_x;
 	switch (currentInput)
 	{
 	case QUIT:
 		run = false;
 		break;
 	case UP:
+		personaje_y_new--;
 		break;
 	case DOWN:
+		personaje_y_new++;
 		break;
 	case LEFT:
+		personaje_x_new--;
 		break;
 	case RIGHT:
+		personaje_x_new++;
+		break;
+	}
+	if (personaje_x_new < 0) {
+		personaje_x_new = MAP_HORIZONTAL - 1;
+	}
+	personaje_x_new = personaje_x_new % MAP_HORIZONTAL;
+
+	if (personaje_y_new < 0) {
+		personaje_y_new = MAP_VERTICAL - 1;
+	}
+	personaje_y_new = personaje_y_new % MAP_VERTICAL;
+	
+	switch (map[personaje_y_new][personaje_x_new])
+	{
+	case TILES::WALL:
+		break;
+	case TILES:: POINT:
+		puntuacion_actual++;
+		map[personaje_y_new][personaje_x_new] = TILES::EMPTY;
+	default:
+		personaje_y = personaje_y_new;
+		personaje_x = personaje_x_new;
 		break;
 	}
 }
-
 void Draw() {
 	system("CLS");
 	for (size_t i = 0; i < MAP_VERTICAL; i++)
 	{
 		for (size_t j = 0; j < MAP_HORIZONTAL; j++)
 		{
-			std::cout << (char)map[i][j];
+			if (i == personaje_y && j == personaje_x){
+				std::cout << PERSONAJE;
+            }
+			else {
+				std::cout << (char)map[i][j];
+			}
 		}
 		std::cout << std::endl;
 	}
